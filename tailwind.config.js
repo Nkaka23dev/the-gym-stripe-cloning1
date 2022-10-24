4aac41fc4f3a6473462ed9247cdb1077202e5159
Nkaka23dev <nkakaeric96@gmail.com>
2022-10-24 05:35:59 +0200

    desktop version

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 module.exports = {
   content: ["./dist/**/*.html"],
   theme: {
-    extend: {},
+    rotate: {
+      '-180': '-180deg',
+       '-90': '-90deg',
+      '-45': '-45deg',
+       '0': '0',
+       '45': '45deg',
+       '90': '90deg',
+      '135': '135deg',
+       '180': '180deg',
+      '270': '270deg',
+     },
+    extend: {
+      colors: {
+        primary:'#0a2540',
+        accentColor: '#00d4ff'
+      },
+      scale: {
+        '160':'2',
+        '175': '3',
+      },
+      skew: {
+        '25': '25deg',
+        '60': '60deg',
+        '-22':'-25deg'
+       },
+       rotate: {
+        '12':'12deg',
+        '5': '6deg'
+       }
+    },
   },
   plugins: [],
 }
